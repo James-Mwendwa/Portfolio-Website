a985a78d8de6872af6674c23e58a95a276b09ac7
James Mwendwa <mwendwajames32@gmail.com>
2022-10-06 22:23:27 +0300

    form validation

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,35 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import Head from "next/head";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    e.target.reset();
+
+    emailjs
+      .sendForm(
+        "service_7sun1h9",
+        "template_233gs2n",
+        form.current,
+        "Sq1FmuU5JhDw9P9YJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message send");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <Head>
@@ -15,15 +42,14 @@ function Contact() {
         </div>
         <h2>Contacts</h2>
         <div className="formPage">
-          <form>
-            <input type="text" required placeholder="Name" />
-            <input type="text" required placeholder="Email" />
-            <textarea
-              cols="30"
-              rows="10"
-              placeholder="Type your message here..."
-            />
-            <button>SUBMIT</button>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" required autoComplete="off" />
+            <label>Email</label>
+            <input type="email" name="user_email" required autoComplete="off" />
+            <label>Message</label>
+            <textarea name="message" required autoComplete="off" />
+            <input type="submit" value="Send" />
           </form>
         </div>
       </div>
