7e99d16876d72f49b4de9a646d6a334364d47f7e
James Mwendwa <mwendwajames32@gmail.com>
2022-10-11 19:18:53 +0300

    added framer motion

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,23 @@ import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import styles from "../styles/Home.module.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: 5,
+    },
+  },
+};
 
 export default function Home() {
   return (
     <>
-      <div className={styles.socials}>
-        <GitHubIcon />
-        <LinkedInIcon />
-      </div>
       <div className={styles.homepage}>
         <Image
           className={styles.heroImage}
@@ -32,12 +41,20 @@ export default function Home() {
           </h2>
           <p>A SOFTWARE ENGINEER BASED IN NAIROBI, KENYA</p>
           <br></br>
-          <button className={styles.button}>
+          <motion.button
+            className={styles.button}
+            variants={buttonVariants}
+            whileHover="hover"
+          >
             DOWNLOAD CV
             <FileDownloadIcon />
-          </button>
+          </motion.button>
         </div>
       </div>
+      {/* <div className={styles.socials}>
+        <GitHubIcon />
+        <LinkedInIcon />
+      </div> */}
       <>
         <About />
         <Skills />
--- pages/works.js
+++ pages/works.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Work.module.css";
 import { projects } from "../lib/data";
+import { motion } from "framer-motion";
 
 function Works() {
   return (
@@ -10,11 +11,11 @@ function Works() {
         <title>Portfolio | Portfolio</title>
         <meta name="keywords" content="work" />
       </Head>
-      <div className={styles.projects}>
+      <motion.div className={styles.projects}>
         <h2 className={styles.title}>PROJECTS</h2>
         {projects.map((project, index) => {
           return (
-            <div className={styles.container} key={index}>
+            <motion.div className={styles.container} key={index}>
               <div className={styles.image}>
                 <Image
                   src={project.image}
@@ -27,10 +28,10 @@ function Works() {
                 <h3>{project.title}</h3>
                 <p>{project.description}</p>
               </div>
-            </div>
+            </motion.div>
           );
         })}
-      </div>
+      </motion.div>
     </>
   );
 }
