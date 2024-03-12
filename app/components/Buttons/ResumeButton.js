b5e460fd13a656adaada306f24b532a1d66603bc
James Mwendwa <mwendwajames32@gmail.com>
2024-03-12 21:40:37 +0300

    (feat):create  resume button

---
+++ app/components/Buttons/ResumeButton.js
@@ -0,0 +1,12 @@
+import { MdArrowOutward } from "react-icons/md";
+
+export default function ResumeButton() {
+  return (
+    <div className="bg-white w-28 px-4 py-2 grid place-items-center font-medium mt-5 rounded-md">
+      <button className="text-base flex items-center justify-between">
+        <p>Resume</p>
+        <MdArrowOutward className="ml-3" />
+      </button>
+    </div>
+  );
+}
