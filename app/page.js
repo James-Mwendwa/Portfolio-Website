bab62cf42690fe0bf41c7392b0eb08d6d3286856
James Mwendwa <mwendwajames32@gmail.com>
2024-03-12 21:41:38 +0300

    (feat): create homepage

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
+import ResumeButton from "@/app/components/Buttons/ResumeButton";
+
 export default function Home() {
   return (
-    <main className="">
-      <h2>Portfolio</h2>
+    <main className="w-full flex flex-wrap px-10 py-20">
+      <div className="w-1/2">
+        <div>
+          <h1 className="text-white text-8xl font-extrabold">James</h1>
+          <h1 className="text-slate-600 text-8xl font-extrabold">Mwendwa</h1>
+        </div>
+
+        <p className="text-2xl mt-10 text-orange-400 font-semibold">
+          FULLSTACK DEVELOPER
+        </p>
+
+        <ResumeButton />
+      </div>
     </main>
   );
 }
